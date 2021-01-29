e1c216f5c519da74b1b2b2d1856f71f1d128ca54
Timur Kushukov <31916838+timqsh@users.noreply.github.com>
2021-01-29 23:12:48 +0500

    Уточнять текущую категорию вопроса (#14)

--- guide/scenes.py
+++ guide/scenes.py
@@ -193,11 +193,14 @@ class AnswerScene(GlobalScene):
         nlu_entities = request.request_body["request"]["nlu"]["entities"]
         nlu_numbers = [e["value"] for e in nlu_entities if e["type"] == "YANDEX.NUMBER"]
         answered_correctly = correct_answer in nlu_numbers
-        print(correct_answer, type(correct_answer))
-        print(nlu_numbers)
         text = question["reply_true"] if answered_correctly else question["reply_false"]
+        next_question_prompt = {
+            "simple": "Задать еще простой вопрос?",
+            "hard": "Задать еще сложный вопрос?",
+            "attention": "Задать еще вопрос на внимательность?",
+        }[question["type"]]
         return self.make_response(
-            f"{text} Задать еще вопрос?",
+            f"{text} {next_question_prompt}",
             buttons=[button("Да"), button("Нет")],
             state={state.QUESTION_TYPE: question["type"]},
         )
