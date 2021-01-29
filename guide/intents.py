f277afbc4e79ab6d891790ffeabb4c0bdb8297d1
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-01-29 19:42:31 +0500

    Revert "fix?"
    This reverts commit a5a9f84a6b5a6946147e9479a9b6ebb2e73ba7c3.

--- guide/intents.py
+++ guide/intents.py
@@ -1,6 +1,6 @@
 START_GAME = "start_game"
 START_TOUR = "start_tour"
-GAME_QUESTION = "question_type"
+GAME_QUESTION = "game_question"
 TELL_ABOUT = "tell_about"
 CONFIRM = "YANDEX.CONFIRM"
 REJECT = "YANDEX.REJECT"
